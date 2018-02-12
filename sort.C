105b1f22bd9717ca7666a0ce05acc0d8df0c0c72
SIVATHANU0708 <35222125+SIVATHANU0708@users.noreply.github.com>
2018-02-12 16:29:29 +0530

    sort.C
    sorted array

---
+++ sort.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+int i,j,temp,a[10],n;
+printf("enter the no of elment:");
+scanf("%d",&n);
+printf("enter the element %d:",n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("the sorted array are ");
+for(i=0;i<=n;i++)
+{
+printf("%d\n",a[i]);
+}
+return 0;
+}
